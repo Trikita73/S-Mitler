70be8b14e876e14e4dfb54587014fd78a3f7d34c
Trikita73 <diachenkoandrii7@gmail.com>
2018-07-10 09:16:14 +0300

    OC2

--- js/common.js
+++ js/common.js
@@ -39,11 +39,11 @@ $(function() {
 	});
 	
 	function carouselService() {
-		$('.carousel-service-item').each(function() {
-			var ths =$(this),
-				thsh = ths.find('.carousel-service-content').outerHeight();
+		$('.carousel-services-item').each(function() {
+			var ths = $(this);
+				thsh = ths.find('.carousel-services-content').outerHeight();
 				ths.find('.carousel-services-image').css('min-height', thsh);
 		});
-	}
+	}carouselService();
 
 });
