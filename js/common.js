e901d1d4ab5e17a5c27c74385eaeeafdb4ac26fd
Trikita73 <diachenkoandrii7@gmail.com>
2018-07-12 18:12:13 +0300

    S-M

--- js/common.js
+++ js/common.js
@@ -58,6 +58,10 @@ $(function() {
 		var ths = $(this);
 		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
 	});
+	$('section .h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+	});
 
 	//-- Add function on Resize Window --//
 	function onResize() {
