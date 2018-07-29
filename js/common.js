bbf2355d3e97ee74d88e76e4d1450fc44c39f13b
Trikita73 <diachenkoandrii7@gmail.com>
2018-07-29 19:38:47 +0300

    Up_Button

--- js/common.js
+++ js/common.js
@@ -78,6 +78,18 @@ $(function() {
 			}
 		}
 	});
+
+	//-- Up-Button --//
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		}else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
 	
 	//-- Image Up Down Carousel --//
 	function carouselService() {
